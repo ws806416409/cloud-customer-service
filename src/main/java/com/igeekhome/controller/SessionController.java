686a874fc0ed640a62deb322a637a5c686f65e08
sunwarm2001 <1015942446@qq.com>
2021-03-24 17:38:49 +0800

    添加会话模块

--- src/main/java/com/igeekhome/controller/SessionController.java
+++ src/main/java/com/igeekhome/controller/SessionController.java
@@ -1,12 +1,21 @@
 package com.igeekhome.controller;
 
 
+import com.igeekhome.biz.impl.CustomerInfoServiceImpl;
+import com.igeekhome.biz.impl.CustomerServiceServiceImpl;
+import com.igeekhome.biz.impl.SessionServiceImpl;
+import com.igeekhome.pojo.CustomerInfo;
+import com.igeekhome.pojo.Session;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,10 +26,29 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @since 2021-03-16
  */
 @Controller
-@Slf4j
 @RequestMapping("/session")
 public class SessionController {
 
+    @Autowired
+    SessionServiceImpl sessionService;
 
+    @Autowired
+    CustomerInfoServiceImpl customerInfoService;
+
+    @RequestMapping("/prelist")
+    public String prelist(Model model){
+
+        List<Session> list = this.sessionService.list();
+        model.addAttribute("list",list);
+        return "/session/session-pre-list";
+    }
+
+    @RequestMapping("/list")
+    public String list(Model model){
+
+        List<CustomerInfo> list = this.customerInfoService.list();
+        model.addAttribute("list",list);
+        return "/session/session-list";
+    }
 }
 
